b4a99862817eff2993b10e4ff0b49a0f7e02347a
Lauri Saarni <lauri.saarni@gmail.com>
2023-09-08 16:57:06 +0300

    Add calendar

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Kalenteri</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+      <iframe src="https://calendar.google.com/calendar/embed?height=500&wkst=2&bgcolor=%23f7f7f6&ctz=Europe%2FHelsinki&mode=AGENDA&showTitle=1&showNav=0&showPrint=0&showTabs=0&showCalendars=0&showTz=0&title=Tapahtumakalenteri&src=bnRjZnZmMjR2cGwwOWRndnBrcHRqYjRwcWNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23616161&amp;hl=fi" style={{borderWidth: 0}} width="485" height="500" frameborder="0" scrolling="no"></iframe>
       </main>
-
-      <Footer />
     </div>
   )
 }
